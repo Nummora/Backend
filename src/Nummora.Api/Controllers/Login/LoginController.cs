e36fb20bb39586dddc6ab15b916009742ce1df2e
Axus00 <fjgt2000@gmail.com>
2025-05-31 11:36:34 -0500

    refactor(Login): Change login ubication into Api

---
+++ src/Nummora.Api/Controllers/Login/LoginController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Nummora.Application.Abstractions;
+using Nummora.Contracts.DTOs;
+
+namespace Nummora.Api.Controllers.Login;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoginController(IUserService _userService) : ControllerBase
+{
+    /// <summary>
+    /// Login
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserDto loginDto)
+    {
+        var result = await _userService.LoginAsync(loginDto);
+        
+        if (!result.IsSuccess)
+            Unauthorized(result.Message);
+        
+        return Ok(result.Message);
+    }
+}
--- src/Nummora.Api/Controllers/Users/UserCreateController.cs
+++ src/Nummora.Api/Controllers/Users/UserCreateController.cs
@@ -18,19 +18,4 @@ public class UserCreateController(IUserService _userService) : ControllerBase
         var result = await _userService.CreateUserAsync(userRegisterDto);
         return Ok(result.Data);
     }
-
-    /// <summary>
-    /// Login
-    /// </summary>
-    /// <returns></returns>
-    [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] UserDto loginDto)
-    {
-        var result = await _userService.LoginAsync(loginDto);
-        
-        if (!result.IsSuccess)
-            Unauthorized(result.Message);
-        
-        return Ok(result.Message);
-    }
 }
